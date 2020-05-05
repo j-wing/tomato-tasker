67f9694ebd646b417a82ade99e501d59e82c542d
Jordon Wing <jordonwii@gmail.com>
2020-05-05 10:52:56 -0700

    Updates

---
+++ precache-manifest.925dab6894c761270eedd980aa47cd31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af56eee4bc31ed542a863d02c658f6e6",
+    "url": "/tomato-tasker/index.html"
+  },
+  {
+    "revision": "4b21f6c700e51ebcce76",
+    "url": "/tomato-tasker/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "42450bea2c4ff41912c4",
+    "url": "/tomato-tasker/static/css/main.357e78c1.chunk.css"
+  },
+  {
+    "revision": "4b21f6c700e51ebcce76",
+    "url": "/tomato-tasker/static/js/2.d6f823dd.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/tomato-tasker/static/js/2.d6f823dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42450bea2c4ff41912c4",
+    "url": "/tomato-tasker/static/js/main.aabbaf6b.chunk.js"
+  },
+  {
+    "revision": "aa87a1a65ec00b3022dc",
+    "url": "/tomato-tasker/static/js/runtime-main.e35a962b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aca82861281b5d2ec134a4f6b3e83239.js"
+  "/tomato-tasker/precache-manifest.925dab6894c761270eedd980aa47cd31.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tomato-tasker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
