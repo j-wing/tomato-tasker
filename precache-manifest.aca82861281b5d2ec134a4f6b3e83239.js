e49e1ae50ab12c3705a8be2dd81585ec46922c13
Jordon Wing <jordonwii@gmail.com>
2020-05-05 10:46:32 -0700

    Updates

---
+++ precache-manifest.aca82861281b5d2ec134a4f6b3e83239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694e76773a3b0b4b5e35cb17850b249c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b21f6c700e51ebcce76",
+    "url": "/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "42450bea2c4ff41912c4",
+    "url": "/static/css/main.357e78c1.chunk.css"
+  },
+  {
+    "revision": "4b21f6c700e51ebcce76",
+    "url": "/static/js/2.d6f823dd.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.d6f823dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42450bea2c4ff41912c4",
+    "url": "/static/js/main.aabbaf6b.chunk.js"
+  },
+  {
+    "revision": "73d573beaa4ae9f59e7d",
+    "url": "/static/js/runtime-main.638e76ad.js"
+  }
+]);
